a17de43d051d9f2299f63862045c589636fb07ac
Pierre SAVIGNY <27277903+Arkanheal@users.noreply.github.com>
2023-09-28 10:57:10 +0200

    Add basic curl command

--- lua/arkan/mappings.lua
+++ lua/arkan/mappings.lua
@@ -1,49 +1,49 @@
-vim.cmd('noremap <silent> // :noh<cr>:call clearmatches()<cr>')
+vim.cmd("noremap <silent> // :noh<cr>:call clearmatches()<cr>")
 
 local map = function(mode, shortcut, command)
-  vim.keymap.set(mode, shortcut, command, { noremap = true, silent = true })
+	vim.keymap.set(mode, shortcut, command, { noremap = true, silent = true })
 end
 
 local nmap = function(shortcut, command)
-  map('n', shortcut, command)
+	map("n", shortcut, command)
 end
 
 local imap = function(shortcut, command)
-  map('i', shortcut, command)
+	map("i", shortcut, command)
 end
 
 local vmap = function(shortcut, command)
-  map('v', shortcut, command)
+	map("v", shortcut, command)
 end
 
 local xmap = function(shortcut, command)
-  map('x', shortcut, command)
+	map("x", shortcut, command)
 end
 
-nmap('<leader>pv', vim.cmd.Ex)
+nmap("<leader>pv", vim.cmd.Ex)
 
 -- Remap CRTL-C to ESC
-imap('<C-c>', '<Esc>')
+imap("<C-c>", "<Esc>")
 
 -- Line moving around
 vmap("<A-j>", ":m '>+1<CR>gv=gv")
 vmap("<A-k>", ":m '<-2<CR>gv=gv")
 
 -- keep search matches in the middle of the window
-nmap('n', 'nzzzv')
-nmap('N', 'Nzzzv')
-nmap('J', 'mzJ`z')
-nmap('<C-d>', '<C-d>zz')
-nmap('<C-u>', '<C-u>zz')
+nmap("n", "nzzzv")
+nmap("N", "Nzzzv")
+nmap("J", "mzJ`z")
+nmap("<C-d>", "<C-d>zz")
+nmap("<C-u>", "<C-u>zz")
 
-xmap('<leader>p', "\"_dP")
+xmap("<leader>p", '"_dP')
 
-nmap('<leader>y', "\"+y")
-vmap('<leader>y', "\"+y")
-nmap('<leader>Y', "\"+Y")
+nmap("<leader>y", '"+y')
+vmap("<leader>y", '"+y')
+nmap("<leader>Y", '"+Y')
 
-nmap('<leader>d', "\"_d")
-vmap('<leader>d', "\"_d")
+nmap("<leader>d", '"_d')
+vmap("<leader>d", '"_d')
 
 nmap("Q", "<nop>")
 
@@ -55,3 +55,7 @@ nmap("<leader>j", "<cmd>lprev<CR>zz")
 nmap("<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 nmap("<leader>x", "<cmd>!chmod +x %<CR>")
 nmap("<leader>m", "<cmd>!make run<CR>")
+
+-- Create command for shell execution
+vim.api.nvim_create_user_command("Curl", "set splitright | vnew | set filetype=json | read !sh # | jq", {})
+nmap("<leader>cur", ":Curl<CR>")
